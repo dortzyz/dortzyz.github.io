2f8a8484e322935c657b8917ea9c9322b4b77b87
do <diegortz25@hotmail.com>
2025-08-08 17:05:05 -0500

    hola mundo js y html

--- js/card-test.js
+++ js/card-test.js
@@ -1,1 +1,13 @@
 /* test de ventana para catalogo */
+
+function holaMundo() {
+    console.log("hola mundo")
+    /* let x = document.getElementsByClassName("title-prime").style.color */
+    /* if (document.getElementsByClassName("nav-box").style.color === "white"){
+        document.getElementsByClassName("title-prime").style.color = "red"
+    } else {
+        document.getElementsByClassName("title-prime").style.color = "red"
+    } */
+    /* document.getElementsByClassName("nav-box").style.color = "red" */
+    
+}
