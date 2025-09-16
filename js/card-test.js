2d3799aee960771bebcf42cffd69cbe2de1cfcf8
dortizy <diegortz25@hotmail.com>
2025-09-16 14:24:52 -0500

    pruebas de card reveal en js

--- js/card-test.js
+++ js/card-test.js
@@ -9,5 +9,23 @@ function holaMundo() {
         document.getElementsByClassName("title-prime").style.color = "red"
     } */
     /* document.getElementsByClassName("nav-box").style.color = "red" */
-    
+    /* const elem = document.querySelector(".grid-section-card");
+    const parent = elem.firstElementChild;
+    console.log(parent); */
+/*
+    // deteccion de click en la pagina
+    document.addEventListener('click', function(event) {
+        // This function will execute whenever a click occurs anywhere on the document.
+        // The 'event' object contains details about the click, including the target element.
+        console.log('Clicked on:', event.target);
+        const elementClick = event.target;
+
+        // Example: Check if the click was outside a specific element
+        const myElement = document.getElementById('mySpecificElement');
+        if (myElement && !myElement.contains(event.target)) {
+            console.log('Clicked outside mySpecificElement');
+            // Perform actions when clicked outside
+        }
+    });
+*/
 }
